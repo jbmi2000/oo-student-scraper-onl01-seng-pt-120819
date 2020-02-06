02f0d43d318c6ac6a2746eaf23644daa299af7ae
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-06 03:37:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
@@ -35,4 +35,3 @@ class Student
   end
 
 end
-
