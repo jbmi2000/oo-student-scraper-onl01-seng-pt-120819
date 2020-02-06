9907bd74e2ae8387046d08ddc02f8fcf568bcc5d
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-06 01:50:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,15 +4,14 @@ require 'nokogiri'
 
 class Scraper
 
-  html 
+  html
 
   def self.scrape_index_page(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
